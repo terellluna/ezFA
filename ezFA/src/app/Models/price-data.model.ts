14d473497c7f1427eed106dfd2c1fccd37cfcf31
Vanessa Duarte <vduarte00@yahoo.com>
2021-08-18 01:08:53 -0500

    added price data model

--- ezFA/src/app/Models/price-data.model.ts
+++ ezFA/src/app/Models/price-data.model.ts
@@ -1,3 +1,17 @@
 export class PriceData {
-    
+    constructor(
+        public date: string,
+        public open: number,
+        public high: number,
+        public low: number,
+        public close: number,
+        public adjClose: number,
+        public volumne: number,
+        public unadjustedVolume: number,
+        public change: number,
+        public changePercent: number,
+        public vwap: number,
+        public label: string,
+        changeOverTime: number
+    ){}
 }
